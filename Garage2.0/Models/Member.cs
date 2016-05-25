931492da4aa04177dc5806a11557fef7eee50020
Andreas Thyrhaug <chessmachine75@gmail.com>
2016-05-25 15:11:23 +0200

    Added regular expression to validate email address.

--- Garage2.0/Models/Member.cs
+++ Garage2.0/Models/Member.cs
@@ -19,6 +19,7 @@ namespace Garage2_5.Models
         [Required]
         public string Phone { get; set; }
         [Required]
+        [RegularExpression(@"^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$")]
         public string Email { get; set; }
     }
 }
